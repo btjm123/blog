469a5529607354341c307474ea9390ce3312739e
Benn Tan <btjm123@Benns-MacBook-Pro.local>
2022-05-29 17:10:53 +0800

    settle

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "Generating Functions",
         "excerpt":"layout: post title: “GENERATING FUNCTIONS” date: 2022-05-29 16:15:00 -0000 categories: functions polynomial   Hello everyone!  ","categories": [],
         "tags": [],
-        "url": "/blog/2022/05/29/GENERATING-FUNCTIONS.html",
+        "url": "/2022/05/29/GENERATING-FUNCTIONS.html",
         "teaser": null
       },{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/blog/jekyll/update/2022/05/29/welcome-to-jekyll.html",
+        "url": "/jekyll/update/2022/05/29/welcome-to-jekyll.html",
         "teaser": null
       }]
